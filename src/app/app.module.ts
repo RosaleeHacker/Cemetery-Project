4c975261de312e8e3db5e544cc40a1ac32d42d40
rosalee-hacker <rah6552@rit.edu>
2018-02-14 02:55:13 -0500

    blog and social media feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Geolocation } from '@ionic-native/geolocation';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { BlogPage } from '../pages/blog/blog';
+import { TimelinePage } from '../pages/timeline/timeline';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -15,8 +15,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    BlogPage,
+    TimelinePage,
     HomePage,
     TabsPage
   ],
@@ -27,8 +27,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    BlogPage,
+    TimelinePage,
     HomePage,
     TabsPage
   ],
