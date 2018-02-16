a1164856f2e5cb036b1e089bb1f35c8d8ced6832
lynsamson <36421496+lynsamson@users.noreply.github.com>
2018-02-16 23:06:26 +0400

    Add files via upload

---
+++ src/pages/vegetation/vegetation.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-vegetation',
+  templateUrl: 'vegetation.html'
+})
+export class VegetationPage {
+
+  constructor(public navCtrl: NavController) {
+  
+  }
+
+}
