ee36a76f97869a1d87f25607eae8f37920ab61b6
lynsamson <36421496+lynsamson@users.noreply.github.com>
2018-02-25 08:05:48 +0400

    Update tabs.ts

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -6,6 +6,8 @@ import { HomePage } from '../home/home';
 import { DataEntryPage } from '../data-entry/data-entry';
 import { VegetationPage } from '../vegetation/vegetation';
 import { AboutPage } from '../about/about';
+import { TombstonesPage } from '../tombstones/tombstones';
+import { HistoryTrailsPage } from '../history-trails/history-trails';
 
 
 @Component({
@@ -18,7 +20,9 @@ export class TabsPage {
   tab3Root = BlogPage;
   tab4Root = DataEntryPage;
   tab5Root = VegetationPage;
-  tab6Root = AboutPage;
+  tab6Root = TombstonesPage;
+  tab7Root = HistoryTrailsPage;
+  tab8Root = AboutPage;
 
   constructor() {
 
