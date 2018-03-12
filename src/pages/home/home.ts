ad9d5c3c4bab4b2d910aeb4cd52a67395d4691ab
lynsamson <36421496+lynsamson@users.noreply.github.com>
2018-03-12 23:02:17 +0400

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -25,63 +25,107 @@ export class HomePage {
     this.loadVegetationMarkers();
   }
 
-  initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 19,
-      center: new google.maps.LatLng(43.129417, -77.639181),
-      mapTypeId: 'roadmap'
-    });
+ initMap() {
+  
+      let options = {timeout:10000, enableletHighAccuracy: true};
+      this.geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.hybrid
+      }
+       let element = this.mapElement.nativeElement;
+       this.map = new google.maps.Map(element, mapOptions);
 
-    //first grave
-    var contentString1 = '<div id="content">' +
-      '<div id="siteNotice">' +
-      '</div>' +
-      '<h1 id="firstHeading" class="firstHeading">John Doe</h1>' +
-      '<div id="bodyContent">' +
-      '<p>1/23/1863-4/09/1930</p>' +
-      '<p>World War I</p>' +
-      '</div>' +
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: this.map.getCenter()
+      });
+     
+     //Necessary for navigation
+      let directionsService = new google.maps.DirectionsService;
+      let directionsDisplay = new google.maps.DirectionsRenderer;
+            
+     //first grave
+      var contentString1 = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">John Doe</h1>'+
+      '<div id="bodyContent">'+
+      '<p>1/23/1863-4/09/1930</p>'+
+      '<p>World War I</p>'+
+      '</div>'+
       '</div>';
 
-    var infowindow1 = new google.maps.InfoWindow({
-      content: contentString1
-    });
-
-    var marker1 = new google.maps.Marker({
-      position: {lat: 43.129313, lng: -77.639573},
-      map: map,
-      title: 'Hello World!',
-    });
-
-    marker1.addListener('click', function () {
-      infowindow1.open(map, marker1);
-    });
-
-    //second grave
-    var contentString2 = '<div id="content">' +
-      '<div id="siteNotice">' +
-      '</div>' +
-      '<h1 id="firstHeading" class="firstHeading">James Doe</h1>' +
-      '<div id="bodyContent">' +
-      '<p>4/22/1893-6/13/1954</p>' +
-      '<p>World War II</p>' +
-      '</div>' +
+      var infowindow1 = new google.maps.InfoWindow({
+    	content: contentString1
+  		});
+      
+   let marker1 = new google.maps.Marker({
+          map: this.map,
+          position: {lat: 25.2750, lng: 55.3681},
+        });
+      
+      marker1.addListener('click', function() {
+    		infowindow1.open(this.map, marker1);
+            directionsDisplay.setMap(this.map);
+ 
+            directionsService.route({
+                origin: latLng,
+                destination: {lat: 25.2750, lng: 55.3681},
+                travelMode: google.maps.TravelMode['DRIVING']}, (res, status) => {
+                
+            if(status == google.maps.DirectionsStatus.OK){
+                directionsDisplay.setDirections(res);
+            } else {
+                console.warn(status);
+            }
+ 
+        }); 
+        
+  		});
+        
+       //second grave
+  	  var contentString2 = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">James Doe</h1>'+
+      '<div id="bodyContent">'+
+      '<p>4/22/1893-6/13/1954</p>'+
+      '<p>World War II</p>'+
+      '</div>'+
       '</div>';
 
-    var infowindow2 = new google.maps.InfoWindow({
-      content: contentString2
-    });
+      var infowindow2 = new google.maps.InfoWindow({
+    	content: contentString2
+  		});
 
-    var marker2 = new google.maps.Marker({
-      position: {lat: 43.129569, lng: -77.639436},
-      map: map,
-      title: 'Hello World!'
-    });
+      let marker2 = new google.maps.Marker({
+          map: this.map,
+          position: {lat: 42.641915, lng: 18.102889},
+        });
 
-    marker2.addListener('click', function () {
-      infowindow2.open(map, marker2);
-    });
-
+        marker2.addListener('click', function() {
+    		infowindow2.open(this.map, marker2);
+            directionsDisplay.setMap(this.map);
+ 
+            directionsService.route({
+                origin: latLng,
+                destination: {lat: 42.641915, lng: 18.102889},
+                travelMode: google.maps.TravelMode['DRIVING']}, (res, status) => {
+ 
+            if(status == google.maps.DirectionsStatus.OK){
+                directionsDisplay.setDirections(res);
+            } else {
+                console.warn(status);
+            }
+ 
+        }); 
+  		});
+        
     //third grave
     var contentString3 = '<div id="content">' +
       '<div id="siteNotice">' +
@@ -132,7 +176,11 @@ export class HomePage {
       infowindow4.open(map, marker4);
     });
 
-  }
+   }, (err) => {
+       console.log(err);
+    });
+   
+ }
 
   loadVegetationMarkers() {
     let vegetationList$: Observable<Vegetation[]>;
