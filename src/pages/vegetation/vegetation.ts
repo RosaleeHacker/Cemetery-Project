f226b4ac7df8f9ba4099e9c4be0867c4a1c40f1f
lynsamson <36421496+lynsamson@users.noreply.github.com>
2018-03-28 19:58:02 +0400

    Update vegetation.ts

--- src/pages/vegetation/vegetation.ts
+++ src/pages/vegetation/vegetation.ts
@@ -3,6 +3,8 @@ import { NavController } from 'ionic-angular';
 import {DatabaseAccessService} from "../../services/data-entry/data-access.service";
 import {Observable} from "rxjs/Observable";
 import {Vegetation} from "../../models/vegetation/vegetation.interface";
+import { NativeAudio } from '@ionic-native/native-audio';
+
 
 @Component({
   selector: 'page-vegetation',
@@ -11,8 +13,9 @@ import {Vegetation} from "../../models/vegetation/vegetation.interface";
 export class VegetationPage {
 
   vegetationList$: Observable<Vegetation[]>;
-
-  constructor(public navCtrl: NavController, private vegetation: DatabaseAccessService) {
+  disableBtn: boolean;
+  
+  constructor(public navCtrl: NavController, private vegetation: DatabaseAccessService, public nativeAudio: NativeAudio) {
     this.vegetationList$ = this.vegetation.getVegetationData().snapshotChanges().map(
       changes => {
         console.log('Changes thingy: ' + changes);
@@ -23,5 +26,18 @@ export class VegetationPage {
       }
     );
   }
+   
+  startAudio() {
+   this.nativeAudio.preloadComplex('test', '../../assets/audio/Downtown.mp3', 1, 1, 0);
+   this.nativeAudio.play('test');
+   this.disableBtn= true;    
+   };
+
+  stopAudio() {
+    this.nativeAudio.stop('test');
+    this.disableBtn= false;    
+
+  };
+
 
 }
